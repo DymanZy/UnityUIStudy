436f55727cd2867dba5716e0f4bac26daaa4a4c8
Dyman <dyman_zy@163.com>
2017-11-22 16:28:35 +0800

    Add CoroutineRunner

---
+++ Assets/Scripts/Common/CoroutineRunner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+public class CoroutineRunner : DontDestorySingletonBehaviour<CoroutineRunner>
+{
+
+    public void Run(IEnumerator coroutine, System.Action finish = null)
+    {
+        if (coroutine == null)
+        {
+            if (finish != null)
+                finish();
+            return;
+        }
+
+        StartCoroutine(coroutine);
+    }
+
+    public IEnumerator run(IEnumerator coroutine, System.Action finish)
+    {
+        yield return StartCoroutine(coroutine);
+        if (finish != null)
+            finish();
+    }
+
+}
+
---
+++ Assets/Scripts/Common/DontDestorySingletonBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DontDestorySingletonBehaviour<T> : ObjectBaseBehavior where T : DontDestorySingletonBehaviour<T>
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance != null) return instance;
+
+            instance = (new GameObject("DontDestroy_" + typeof(T).Name)).AddComponent<T>();
+            DontDestroyOnLoad(instance.gameObject);
+            return instance;
+        }
+    }
+}
+
