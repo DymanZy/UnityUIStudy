c2547176cd3033e83aa09ee9256fb2910adb9986
Dyman <dyman_zy@163.com>
2017-11-23 15:34:03 +0800

    test click event pass

--- Assets/Scripts/Base/CustomButton.cs
+++ Assets/Scripts/Base/CustomButton.cs
@@ -1,25 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 
-
+[RequireComponent(typeof(uGUIEventSystemSupporter)),
+ RequireComponent(typeof(Image))]
 public class CustomButton : Button
 {
+
+    [SerializeField]
+    private uGUIEventSystemSupporter EventSupporter;
+
+    private bool callDelegateFlag = true;
+    public void setDelegateFlag(bool flag) {
+        callDelegateFlag = flag;
+    }
 
     public CallBackSupporter<uGUIEventSystemSupporter> ClickCallBack = new CallBackSupporter<uGUIEventSystemSupporter>();
     public CallBackSupporter<CustomButton> ClickCallBackOfButtonComponent = new CallBackSupporter<CustomButton>();
 
-    public CallBackSupporter<uGUIEventSystemSupporter> LongPressCallBack = new CallBackSupporter<uGUIEventSystemSupporter>();
-    public CallBackSupporter<CustomButton> LongPressCallBackOfButtonComponent = new CallBackSupporter<CustomButton>();
-
 
     private bool isInitialized = false;
-
-
 
     protected override void Awake() {
         if (isInitialized) return;
 
-     
+        EventSupporter = transform.gameObject.GetComponent<uGUIEventSystemSupporter>();
+
+        EventSupporter.AddCallBack<IPointerClickHandler>(ClickEvent);
+
+        // another wait to do
+
+        isInitialized = true;
     }
+
+    private void ClickEvent(uGUIEventSystemSupporter s, BaseEventData data) {
+
+        if (callDelegateFlag) {
+            ClickCallBack.run(s);
+            ClickCallBackOfButtonComponent.run(this);
+        }
+    }
+
 }
---
+++ Assets/Scripts/DraggablePanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggablePanel : MonoBehaviour {
+
+    [SerializeField]
+    private CustomButton button;
+    [SerializeField]
+    private CustomButton testButton;
+
+    // Use this for initialization
+    void Start () {
+
+        button.ClickCallBack.add(ButtonEvent_Test);
+        testButton.ClickCallBackOfButtonComponent.add(ButtonEvent_CustomTest);
+        button.setDelegateFlag(false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void ButtonEvent_Test(uGUIEventSystemSupporter s) {
+        Common.LogUtil.Log("Click Button Test");
+    }
+
+    private void ButtonEvent_CustomTest(CustomButton b) {
+        Common.LogUtil.Log("Click Button Custom Test");
+    }
+
+}
