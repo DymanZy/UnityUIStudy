1f45b8771e127f5e92998325451f5d1e08e9acb0
Dyman <dyman_zy@163.com>
2017-12-19 15:37:08 +0800

    test

---
+++ Assets/Editor/CommandBuild.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class CommandBuild {
+	public static void BuildAndroid() {
+		string[] levels = { "Assets/Scenes/Drag And Drop.unity" };
+		string savePath = "./Output/AndroidTest_" + DateTime.Now.ToString("yyMMdd_HHmm") + ".apk";
+		BuildPipeline.BuildPlayer(levels, savePath, BuildTarget.Android, BuildOptions.None);
+	}
+}
